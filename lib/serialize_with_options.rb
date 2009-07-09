ed83d56f2a8bf3bb37e772cbf1c40770b1d387f1
David Eisinger <david.eisinger@viget.com>
2009-07-09 13:32:59 -0400

    Added serialization_configuration class method

--- lib/serialize_with_options.rb
+++ lib/serialize_with_options.rb
@@ -2,59 +2,50 @@ module SerializeWithOptions
   def serialize_with_options(&block)
     config = Config.new
     config.instance_eval(&block)
+    @configuration = config.data
 
-    @serialization_options = config.options
-
-    extend ClassMethods
     include InstanceMethods
   end
 
+  def serialization_configuration
+    @configuration.try(:dup) || {}
+  end
+
   def serialization_options
-    @serialization_options || {}
-  end
+    @options ||= returning(serialization_configuration) do |opts|
+      includes = opts.delete(:includes)
 
-  class Config
-    attr_accessor :options
-
-    def initialize
-      @options = {}
-    end
-
-    def methods(*args)
-      @options[:methods] = args
-    end
-
-    def includes(*args)
-      @options[:include] = args
-    end
-
-    def except(*args)
-      @options[:except] = args
-    end
-  end
-
-  module ClassMethods
-    def configure_includes
-      opts = serialization_options
-
-      if opts[:include].is_a? Array
-        opts[:include] = opts[:include].inject({}) do |hash, class_name|
+      if includes
+        opts[:include] = includes.inject({}) do |hash, class_name|
           klass = class_name.to_s.singularize.capitalize.constantize
-          hash[class_name] = klass.serialization_options.dup.merge(:include => nil)
+          hash[class_name] = klass.serialization_configuration
+          hash[class_name][:include] = nil if hash[class_name].delete(:includes)
           hash
         end
       end
     end
   end
 
+  class Config
+    undef_method :methods
+
+    attr_reader :data
+
+    def initialize
+      @data = {}
+    end
+
+    def method_missing(method, *args)
+      @data[method] = args
+    end
+  end
+
   module InstanceMethods
     def to_xml(opts = {})
-      self.class.configure_includes
       super(self.class.serialization_options.merge(opts))
     end
 
     def to_json(opts = {})
-      self.class.configure_includes
       super(self.class.serialization_options.merge(opts))
     end
   end
